2dce9d5810da5ad60b3989e1c249bd52c238c1ea
Camilo Andres Ferrer bustos <caferrerb@MacBook-Pro-de-Camilo-2.local>
2020-09-08 19:31:56 -0500

    jenkins pipelines

---
+++ jenkins/pipeline-parametrizado.groovy
@@ -0,0 +1,52 @@
+pipeline {
+    agent any
+    stages {
+        stage("Checkout app-code") {
+            steps {
+               dir('app') {
+                    git url:"https://github.com/caferrerbeam/estudiantes-ms" , branch: "develop"
+                } 
+            }
+        }
+         
+         stage("Checkout deploy-code") {
+            steps {
+               dir('deploy') {
+                    git url:"https://github.com/caferrerbeam/seminariodocker.git" , branch: "jenkins"
+                } 
+            }
+        }
+        
+        stage("Build image") {
+            steps {
+                dir('app') {
+                    script {
+                        dockerImage = docker.build("eamquindio/estudiante-ms:1.0")
+                    }
+                }
+            }
+        }
+        stage("Push image") {
+            steps {
+                script {
+                    docker.withRegistry('', 'dockerhubclase') {
+                    dockerImage.push()
+                    }
+                }
+            }
+        }
+
+        stage('Deploy') {
+            steps{
+                step([$class: 'KubernetesEngineBuilder', 
+                        projectId: "nice-root-288300",
+                        clusterName: "cluster-camilo",
+                        zone: "us-west1-a",
+                        manifestPattern: 'deploy/k8/estudiantes/',
+                        credentialsId: "seminario",
+                        verifyDeployments: true])
+            }
+        }
+
+    }
+}
